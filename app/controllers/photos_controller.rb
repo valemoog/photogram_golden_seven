d47b614643636315676c8633e3345c381dcbc67b
valemoog <valentinadiaz@wireless-s1-so-150-50-19.uchicago.edu>
2017-04-28 10:37:22 -0500

    Cambios

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,51 @@
+class PhotosController <ApplicationController
+
+  def index
+    @list_of_all_my_photos = Photo.all
+    render("photos/index.html.erb")
+  end
+
+  def new_form
+
+    render("photos/new_form.html.erb")
+  end
+
+  def create_row
+    url= params[:da_source]
+    cap = params[:da_caption]
+    new_photo = Photo.new
+    new_photo.source = url
+    new_photo.caption = cap
+    new_photo.save
+
+    redirect_to("/photos/#{new_photo.id}")
+  end
+
+  def show
+    the_id = params[:id]
+    @my_photo=Photo.find(the_id)
+    render("photos/show.html.erb")
+
+  end
+
+  def update_row
+
+    render("photos/update_row.html.erb")
+
+  end
+
+  def edit_form
+
+
+    render("photos/edit_form.html.erb")
+
+  end
+
+  def destroy_row
+    render("photos/destroy_row.html.erb")
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  get("photos/new", {:controller => "photos", :action => "new_form"})
+  get("create_photo", {:controller => "photos", :action => "create_row"})
+
+  get("photos", {:controller => "photos", :action => "index"})
+  get("photos/:id", {:controller => "photos", :action => "show"})
+
+  get("photos/:id/edit", {:controller => "photos", :action => "edit_form"})
+  get("update_photo/:id", {:controller => "photos", :action => "update_row"})
+
+  get("delete_photo/:id", {:controller => "photos", :action => "destroy_row"})
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
